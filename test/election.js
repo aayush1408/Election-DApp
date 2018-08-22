d354ec3f46cc05898ff954f3ee1be2feb979042f
aayush1408 <aayushjaiswal0114@gmail.com>
2018-08-22 12:49:54 +0530

    test wriiten

---
+++ test/election.js
@@ -0,0 +1,27 @@
+var Election = artifacts.require("./Election.sol");
+
+contract("Election",function(accounts){
+    var electionInstance;
+    it("intializes with 2 candidates",function(){
+        return Election.deployed().then(function(instance){
+            return instance.noOfCandidates();
+        }).then(function(count){
+            assert.equal(count,2);
+        });
+    });
+    it("it initalizes the candidates with the correct values",function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            return electionInstance.candidates(1);
+        }).then(function(candidate){
+            assert.equal(candidate[0],1,"contains the correct id");
+            assert.equal(candidate[1],"Candidate 1","contains the correct name");
+            assert.equal(candidate[2],0,"contains the total votes");
+            return electionInstance.candidates(2);
+        }).then(function(candidate){
+            assert.equal(candidate[0],2,"contains the correct id");
+            assert.equal(candidate[1],"Candidate 2","contains the correct name");
+            assert.equal(candidate[2],0,"contains the total votes");
+        });
+    });
+});
