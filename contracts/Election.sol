23ed254677a147efcbb54832fdd8d78c150272a1
aayush1408 <aayushjaiswal0114@gmail.com>
2018-08-22 12:27:36 +0530

    contract updated

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,8 +1,23 @@
 pragma solidity ^0.4.24;
 
 contract Election{
-    string public candidate;
-       function Election () public {
-           candidate = "Candidate 1";
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    mapping(uint => Candidate) public candidates;
+
+    uint public noOfCandidates;
+
+    function Election() public{
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        noOfCandidates++;
+        candidates[noOfCandidates] = Candidate(noOfCandidates,_name,0);
+
     }
 }
